445cd188080a95b35da4faad38ca6b7f5c0429e8
Fred Hatfull <git@admiralfred.com>
2016-12-23 15:24:39 -0500

    Also update default source url to hit github

--- attributes/source.rb
+++ attributes/source.rb
@@ -12,5 +12,5 @@ include_attribute 'atheme-services::default'
 default['atheme-services']['source']['enable_contrib_modules'] = false
 default['atheme-services']['source']['install_location'] = '/opt/atheme/atheme-services'
 default['atheme-services']['source']['unpack_location'] = '/opt/atheme/atheme-source'
-default['atheme-services']['source']['url'] = "http://atheme.net/downloads/atheme-services-#{node['atheme-services']['version']}.tar.bz2"
+default['atheme-services']['source']['url'] = "https://github.com/atheme/atheme/archive/atheme-services-#{node['atheme-services']['version']}.tar.gz"
 
