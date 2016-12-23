011a8d07fb1de8f008214cdc8ed60d0f1aeef024
Fred Hatfull <git@admiralfred.com>
2016-12-23 14:49:33 -0500

    Version 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'fred.hatfull@gmail.com'
 license          'MIT'
 description      'Installs/Configures atheme-services'
 long_description 'Installs/Configures atheme-services'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'build-essential'
 
